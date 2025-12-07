17de1c39bb43f2eea5ae06c1cc8d67335c12fe49
AqulineBenny <B00166537@mytudublin.ie>
2025-12-07 18:03:30 +0000

    Delete

--- app/api/orders/stats/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-    console.log("in the orders stats api page");
-    
-    return Response.json({
-        "labels": ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-        "sales": [1200, 1900, 1500, 2200, 1800, 2500]
-    });
-}
